85b07aae4eeccc450ad1a5d442eae94bc5dda433
Eofre <yegorgordinskiy2002@gmail.com>
2023-08-12 18:34:56 +0800

    feat: create a EmployeePage page

--- src/pages/EmployeePage/EmployeePage.tsx
+++ src/pages/EmployeePage/EmployeePage.tsx
@@ -1,6 +1,24 @@
 import { type FC } from 'react';
 import classes from './EmployeePage.module.scss';
 
+import { Container } from '../../components/Container';
+import { EmployeeCard } from '../../components/EmployeeCard';
+import employees from '../../data/employees';
+import { FeedbackList } from '../../components/FeedbackList';
+import reviews from '../../data/reviews';
+
 export const EmployeePage: FC = () => {
-  return <section className={classes.employeePage}></section>;
+  return (
+    <section className={classes.employeePage}>
+      <Container maxWidth={1200}>
+        <div className={classes.wrapper}>
+          <EmployeeCard employee={employees[0]} />
+          <div className={classes.feedback}>
+            <h3 className={classes.title}>Отзывы</h3>
+            <FeedbackList reviews={reviews} />
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
 };
