bb6dc98a467d0236f33b57fa236849a87662c0a9
Eofre <yegorgordinskiy2002@gmail.com>
2023-08-12 18:35:55 +0800

    feat: create a StaffPage page

--- src/pages/StaffPage/StaffPage.tsx
+++ src/pages/StaffPage/StaffPage.tsx
@@ -1,6 +1,15 @@
 import { type FC } from 'react';
 import classes from './StaffPage.module.scss';
+import { EmployeeList } from '../../components/EmployeeList';
+import employees from '../../data/employees';
+import { Container } from '../../components/Container';
 
 export const StaffPage: FC = () => {
-  return <section className={classes.staffPage}></section>;
+  return (
+    <section className={classes.staffPage}>
+      <Container maxWidth={1200}>
+        <EmployeeList list={employees} />
+      </Container>
+    </section>
+  );
 };
