bc402140d3f211da47bc23c68411fb29b3199141
Eofre <yegorgordinskiy2002@gmail.com>
2023-08-12 18:32:43 +0800

    feat: create a EmployeeCard component

--- src/components/EmployeeCard/EmployeeCard.tsx
+++ src/components/EmployeeCard/EmployeeCard.tsx
@@ -1,8 +1,27 @@
 import { type FC } from 'react';
 import classes from './EmployeeCard.module.scss';
+import { type IEmployee } from '../../types/types';
+import human from '../../assests/images/human.jpg';
+import { useNavigate } from 'react-router';
+import { Rating } from '@mui/material';
 
-interface EmployeeCardProps {}
+interface EmployeeCardProps {
+  employee: IEmployee;
+}
 
-export const EmployeeCard: FC<EmployeeCardProps> = () => {
-  return <div className={classes.employeeCard}></div>;
+export const EmployeeCard: FC<EmployeeCardProps> = ({ employee }) => {
+  const navigate = useNavigate();
+  const handlerClick = (): void => {
+    navigate(`/employee/${employee.id}`);
+  };
+  return (
+    <div className={classes.employeeCard} onClick={handlerClick}>
+      <img className={classes.img} src={human} alt="" />
+      <div className={classes.content}>
+        <span className={classes.text}>{employee.fullName}</span>
+        <Rating readOnly name="size-large" defaultValue={4} size="small" />
+        <span className={classes.position}>{employee.position}</span>
+      </div>
+    </div>
+  );
 };
