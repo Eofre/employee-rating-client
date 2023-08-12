3a3a021de970fff6a0f8ad39cdd98f1ac6298039
Eofre <yegorgordinskiy2002@gmail.com>
2023-08-12 18:26:03 +0800

    feat: create a Header component

--- src/components/HeaderEmployee/HeaderEmployee.tsx
+++ src/components/HeaderEmployee/HeaderEmployee.tsx
@@ -1,18 +1,21 @@
 import { type FC } from 'react';
-import classes from './Header.module.scss';
-import { Logo } from '../Logo';
+import classes from './HeaderEmployee.module.scss';
 import { Container } from '../Container';
-import { SearchBar } from '../SearchBar';
+import { Logo } from '../Logo';
+import { VscFeedback } from 'react-icons/vsc';
 import { TbHelpSquareRounded } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
-export const Header: FC = () => {
+export const HeaderEmployee: FC = () => {
   return (
     <header className={classes.header}>
       <Container maxWidth={1200}>
         <div className={classes.wrapper}>
           <Logo />
-          <SearchBar label="Сотрудник..." value="fdf" />
+          <div className={classes.nav}>
+            <VscFeedback size={25} />
+            <Link to="/employee/1/feedback">Оставить отзыв</Link>
+          </div>
           <Link className={classes.nav} to="#">
             <TbHelpSquareRounded size={25} /> Помощь
           </Link>
--- src/components/HeaderFeedback/HeaderFeedback.tsx
+++ src/components/HeaderFeedback/HeaderFeedback.tsx
@@ -1,18 +1,22 @@
 import { type FC } from 'react';
-import classes from './Header.module.scss';
-import { Logo } from '../Logo';
+import classes from './HeaderFeedback.module.scss';
 import { Container } from '../Container';
-import { SearchBar } from '../SearchBar';
-import { TbHelpSquareRounded } from 'react-icons/tb';
-import { Link } from 'react-router-dom';
+import { Logo } from '../Logo';
+import { PiKeyReturn } from 'react-icons/pi';
 
-export const Header: FC = () => {
+import { Link } from 'react-router-dom';
+import { TbHelpSquareRounded } from 'react-icons/tb';
+
+export const HeaderFeedback: FC = () => {
   return (
     <header className={classes.header}>
       <Container maxWidth={1200}>
         <div className={classes.wrapper}>
           <Logo />
-          <SearchBar label="Сотрудник..." value="fdf" />
+          <Link className={classes.nav} to="/employee/1">
+            <PiKeyReturn size={25} />
+            На страницу сотрудника
+          </Link>
           <Link className={classes.nav} to="#">
             <TbHelpSquareRounded size={25} /> Помощь
           </Link>
